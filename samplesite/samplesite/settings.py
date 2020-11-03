027020a3eae674d5f47ac8d1e7250d09a51fece6
ArystanIgen <arystan.2000@mail.ru>
2020-11-04 04:07:36 +0600

    set

--- samplesite/samplesite/settings.py
+++ samplesite/samplesite/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     'bboard.apps.BboardConfig',
     'users.apps.UsersConfig',
     'bootstrap4',
-    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -63,7 +62,6 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.request',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
